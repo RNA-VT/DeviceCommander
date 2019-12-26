598dff84552853a14de1531429efa1d87089a721
Tushar Shah <twoshark@gmail.com>
2019-12-26 12:46:48 -0500

    update naming now that we can get hostnames from k8s

--- src/nodecluster/nodeinfo.go
+++ src/nodecluster/nodeinfo.go
@@ -6,18 +6,19 @@ import (
 
 //NodeInfo Information/Metadata about node
 type NodeInfo struct {
-	NodeID     int
-	NodeIPAddr string
-	Port       string
+	NodeID int
+	Host   string
+	Port   string
 }
 
 /*String Just for pretty printing the node info */
 func (node *NodeInfo) String() string {
-	return "NodeInfo:{ nodeId:" + strconv.Itoa(node.NodeID) + ", nodeIpAddr:" + node.NodeIPAddr + ", port:" + node.Port + " }"
+	//strings.Split(myIP[0].String(), "/")[0]
+	return "NodeInfo:{ nodeId:" + strconv.Itoa(node.NodeID) + ", Host:" + node.Host + ", port:" + node.Port + " }"
 }
 
 //ToFullAddress returns a network address including the ip address and port that this node is listening on
 func (node *NodeInfo) ToFullAddress() string {
 	/* Just for pretty printing the node info */
-	return node.NodeIPAddr + ":" + node.Port
+	return node.Host + ":" + node.Port
 }
