8ec4f9f5040ead6604ca8aa64be8bdd3eb074f22
Tushar Shah <twoshark@gmail.com>
2020-01-04 16:22:53 -0500

    stubbed out open close, added enum for NC NO

--- src/component/solenoid.go
+++ src/component/solenoid.go
@@ -1,1 +1,26 @@
 package component
+
+// Solenoid - base component + solenoid specific metadata
+type Solenoid struct {
+	BaseComponent
+	Voltage int
+	Type    SolenoidType
+}
+
+func (s *Solenoid) open(duration int) {
+
+}
+
+func (s *Solenoid) close(delay int) {
+
+}
+
+// SolenoidType is an enum
+type SolenoidType int
+
+const (
+	// NormallyClosed represents a solenoid that does not allow flow without power
+	NormallyClosed = iota + 1
+	// NormallyOpen represents a solenoid that is allows flow without power
+	NormallyOpen
+)
