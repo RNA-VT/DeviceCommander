1987d23a11f5a39fbc1206719eeee65102c7ad4d
Tushar Shah <twoshark@gmail.com>
2020-01-18 16:33:46 -0500

    wip mock system, added component interface

--- src/component/basecomponent.go
+++ src/component/basecomponent.go
@@ -1,45 +1,26 @@
 package component
 
-import (
-	// "fmt"
-	// "encoding/json"
-	// "strings"
-	"strconv"
-
-	"github.com/stianeikeland/go-rpio/v4"
-)
-
 /*BaseComponent object definition */
 type BaseComponent struct {
 	UID       int
 	Enabled   bool
 	Name      string `yaml:"name"`
 	HeaderPin int    `yaml:"header_pin"`
-	GPIO      Gpio   `yaml:"gpio"`
 }
 
-//Enable - make this component available to command
-func (c *BaseComponent) Enable(init bool) {
-	c.Enabled = true
-	if init {
-		c.GPIO.InitializePin(c.HeaderPin, false)
-	}
+/*
+GetBase allows us to share the properties in this type across all components
+	and forces components to obey the Component interface{}
+*/
+func (c *BaseComponent) GetBase() *BaseComponent {
+	return c
 }
 
-//Disable - force this component to an off or safe state and make it unavaible to command
-func (c *BaseComponent) Disable() {
-	c.Enabled = false
-	c.GPIO.Pin.Low()
-}
-
-/*CurrentStateString just for pretty printing the device info */
-func (c *BaseComponent) CurrentStateString() string {
-	state := "OFF"
-
-	if c.GPIO.Pin.Read() == rpio.High {
-		state = "ON"
-	}
-
-	message := "[" + strconv.Itoa(c.UID) + "] is " + state
-	return message
+//Component is an interface shared by the components we can control with a raspi
+type Component interface {
+	Init() error
+	String() string
+	State() string
+	Enable(bool)
+	Disable()
 }
