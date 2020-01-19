64b0c70639ad5c6541303965584433c207d20ed5
Tushar Shah <twoshark@gmail.com>
2020-01-18 19:30:30 -0500

    add error log for init on micontroller class and remove duplicate logging in gpio init

--- src/microcontroller/microcontroller.go
+++ src/microcontroller/microcontroller.go
@@ -56,6 +56,7 @@ func (m *Microcontroller) LoadSolenoids() error {
 	log.Print("Performing Initializations: ")
 	err = m.Init()
 	if err != nil {
+		log.Printf("Error During Initialization: #%v ", err)
 		return err
 	}
 	return nil
