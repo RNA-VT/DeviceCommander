66f271745a48cfee2e8d5fbfaf2c639c3d8e7ad2
Olin <olin@Olins-MBP.hsd1.co.comcast.net>
2022-06-27 20:02:38 -0600

    cleanup

--- frontend/src/pages/store.tsx
+++ frontend/src/pages/store.tsx
@@ -2,28 +2,20 @@ import { atom, selector } from 'recoil';
 import GetDevicesMethod from '../api/method/GetDevices';
 import Device from '../api/device/Device';
 
-interface PageData {
-  title: string
-  index: string
-}
-
-// export const DevicesState = atom<Device[]>({
-//   key: "devices",
-//   default: [],
-// });
-
 export const DevicesState = selector<Device[]>({
   key: 'devices',
   get: async () => {
-    // const response = await myDBQuery({
-    //   userID: get(currentUserIDState),
-    // });
     const apiMethod = new GetDevicesMethod('http://localhost:8001');
     const devices = await apiMethod.do();
 
     return devices;
   },
 });
+
+interface PageData {
+  title: string
+  index: string
+}
 
 export const PageState = atom<PageData>({
   key: 'pageState',
